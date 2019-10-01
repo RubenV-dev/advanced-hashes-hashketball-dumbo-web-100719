c02049520a61bb5ef9ee2a2e8ff5551f23593a20
Ruben Vallejo <r.will16@hotmail.com>
2019-10-01 18:17:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -237,5 +237,14 @@ def big_shoe_rebounds
       end
     end
   end
+  game_hash.each do |location, location_hash|
+    playas = location_hash[:players]
+    playas.each do |name_hash|
+      name_hash.each do |(name, stat_hash)|
+        p name
+        p stat_hash
+      end
+    end
+  end
    
 end
