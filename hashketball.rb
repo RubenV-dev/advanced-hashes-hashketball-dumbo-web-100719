036e7ab727afabda740e4dcd09f80b082dab8767
Ruben Vallejo <r.will16@hotmail.com>
2019-10-01 18:02:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -230,7 +230,7 @@ def big_shoe_rebounds
       name_hash.each do |(name, stat_hash)|
         shoe_size = stat_hash[:shoe]
         stat_hash.reduce(0) do |memo, (key, value)|
-          if shoe_size > memo
+          if shoe_size >= memo
             memo = shoe_size
           end
           memo
